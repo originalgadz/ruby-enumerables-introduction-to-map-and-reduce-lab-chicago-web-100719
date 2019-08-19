942d63ea59dffd19bb38cf99941b765e08bd863a
Glenn A Doherty <gadz@earthlink.net>
2019-08-19 20:03:09 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
-# My Code here....
+def map_to_negativize (source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array[index] = source_array[index] * -1
+  end
+  new_array
+end
+
+def map_to_no_change (source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array[index] = source_array[index]
+  end
+  new_array
+end
+
+def map_to_double (source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array[index] = source_array[index] * 2
+  end
+  new_array
+end
+
+def map_to_square (source_array)
+  new_array = []
+  source_array.length.times do |index|
+    new_array[index] = source_array[index] ** 2
+  end
+  new_array
+end
